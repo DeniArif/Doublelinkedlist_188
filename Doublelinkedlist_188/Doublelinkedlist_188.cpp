734d08b938c7a32bac07af86d4face29fe9170a9
DeniArif <deniarifwibowo82@gmail.com>
2023-05-26 11:12:20 +0700

    comit

--- Doublelinkedlist_188/Doublelinkedlist_188.cpp
+++ Doublelinkedlist_188/Doublelinkedlist_188.cpp
@@ -33,4 +33,19 @@ void DoubleLinkedList::addNode() {
 	int nim;
 	string nm;
 	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm; // step 2
+
+	/*insert a mode in the beginning of a doubly - Linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed " << endl;
+			return;
+		}
+		newNode->next = START;
+	}
 }
